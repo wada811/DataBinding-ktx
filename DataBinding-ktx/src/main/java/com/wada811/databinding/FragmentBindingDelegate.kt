34bc70458e571e4a07791a45dad327da87f3f6b9
wada811 <at.wada811@gmail.com>
2019-03-20 09:36:12 +0900

    Fix memory leak

--- DataBinding-ktx/src/main/java/com/wada811/databinding/FragmentBindingDelegate.kt
+++ DataBinding-ktx/src/main/java/com/wada811/databinding/FragmentBindingDelegate.kt
@@ -5,22 +5,42 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class FragmentBindingDelegate<T : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int
 ) : ReadOnlyProperty<Fragment, T> {
-    private var cached: T? = null
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T =
-        cached ?: DataBindingUtil.inflate<T>(
+    private var binding: T? = null
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        if (binding != null) {
+            return binding!!
+        }
+        binding = DataBindingUtil.inflate(
             thisRef.layoutInflater,
             layoutResId,
             thisRef.requireActivity().findViewById(thisRef.id) as? ViewGroup,
             false
         )
-            .also { it.lifecycleOwner = thisRef.viewLifecycleOwner }
-            .also { cached = it }
+        thisRef.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onViewCreated() {
+                thisRef.lifecycle.removeObserver(this)
+                thisRef.viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+                    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                    fun onDestroyView() {
+                        thisRef.viewLifecycleOwner.lifecycle.removeObserver(this)
+                        binding = null
+                    }
+                })
+                binding!!.lifecycleOwner = thisRef.viewLifecycleOwner
+            }
+        })
+        return binding!!
+    }
 }
 
 fun <T : ViewDataBinding> Fragment.bind(@LayoutRes layoutResId: Int): FragmentBindingDelegate<T> =
