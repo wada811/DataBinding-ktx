fe8d4621ebbcb8dba1d090e613a5ed48d22421e3
wada811 <at.wada811@gmail.com>
2019-05-12 01:03:54 +0900

    Initialize binding automaticaly after onCreate unless initialized

--- DataBinding-ktx/src/main/java/com/wada811/databinding/ActivityBindingDelegate.kt
+++ DataBinding-ktx/src/main/java/com/wada811/databinding/ActivityBindingDelegate.kt
@@ -4,19 +4,33 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class ActivityBindingDelegate<T : ViewDataBinding>
 internal constructor(
+    activity: FragmentActivity,
     @LayoutRes private val layoutResId: Int
 ) : ReadOnlyProperty<FragmentActivity, T> {
-    private var binding: T? = null
-    override operator fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T =
-        binding ?: DataBindingUtil.setContentView<T>(thisRef, layoutResId)
-            .also { it.lifecycleOwner = thisRef }
-            .also { binding = it }
+    init {
+        activity.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                activity.lifecycle.removeObserver(this)
+                binding.executePendingBindings() // initialize binding after onCreate unless initialized
+            }
+        })
+    }
+
+    private val binding: T by lazy(LazyThreadSafetyMode.NONE) {
+        DataBindingUtil.setContentView<T>(activity, layoutResId).also { it.lifecycleOwner = activity }
+    }
+
+    override operator fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T = binding
 }
 
 fun <T : ViewDataBinding> FragmentActivity.bind(@LayoutRes layoutResId: Int): ActivityBindingDelegate<T> =
-    ActivityBindingDelegate(layoutResId)
+    ActivityBindingDelegate(this, layoutResId)
